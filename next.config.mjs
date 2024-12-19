3cb8d0070db7e17af687939a57ea58ac309c70a7
Alec Chen <fkalec@gmail.com>
2024-12-19 14:42:36 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Silence warnings
-    // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
-    webpack: (config) => {
-      config.externals.push('pino-pretty', 'lokijs', 'encoding');
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  // Silence warnings
+  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
