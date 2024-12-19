735babe8d36bd3d3b6105e9f0c2e70b92a89f58e
Alec Chen <fkalec@gmail.com>
2024-12-19 11:46:06 -0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import ImageSvg from './svg/Image';
+import OnchainkitSvg from './svg/OnchainKit';
+
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black">
+      <main className="flex-grow flex items-center justify-center">
+        <div className="max-w-4xl w-full p-4">
+          <div className="w-1/3 mx-auto mb-6">
+            <ImageSvg />
+          </div>
+          <div className="flex justify-center mb-6">
+            <a target="_blank" rel="_template" href="https://onchainkit.xyz">
+              <OnchainkitSvg className="dark:text-white text-black" />
+            </a>
+          </div>
+          <div className="flex flex-col items-center">
+            Hello world!
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
