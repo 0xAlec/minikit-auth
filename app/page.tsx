311f00d9461c34cc4a46f00fa5490b1984cb5aed
Alec Chen <fkalec@gmail.com>
2024-12-19 14:31:08 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
 'use client';
 
-import ImageSvg from './svg/Image';
-import OnchainkitSvg from './svg/OnchainKit';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [message, setMessage] = useState<any>('Waiting for messages...');
+
+  useEffect(() => {
+    // Listen for messages from parent window
+    const handleMessage = (event: MessageEvent) => {
+      setMessage(JSON.stringify(event.data, null, 2));
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    // Signal to parent that iframe is ready
+    window.parent.postMessage({ type: 'IFRAME_READY' }, '*');
+
+    // Cleanup listener on unmount
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black">
       <main className="flex-grow flex items-center justify-center">
         <div className="max-w-4xl w-full p-4">
-          <div className="w-1/3 mx-auto mb-6">
-            <ImageSvg />
-          </div>
-          <div className="flex justify-center mb-6">
-            <a target="_blank" rel="_template" href="https://onchainkit.xyz">
-              <OnchainkitSvg className="dark:text-white text-black" />
-            </a>
-          </div>
           <div className="flex flex-col items-center">
-            Hello world!
+            <pre className="whitespace-pre-wrap break-words">
+              {message}
+            </pre>
           </div>
         </div>
       </main>
